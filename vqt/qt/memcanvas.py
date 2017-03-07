d7d1a6079db3454c89baf575cdc3a6b43e3b1017
serj <bat.serjo@gmail.com>
2017-03-07 23:12:55 +0200

    Primitive memory canvas created.

--- vqt/qt/memcanvas.py
+++ vqt/qt/memcanvas.py
@@ -1,50 +1,25 @@
-import html
-
-from PyQt5 import QtWebEngineWidgets, QtWebChannel
-
 from vqt.main import *
 from vqt.common import *
-
-import vqt.qt.html as e_q_html
-import vqt.qt.jquery as e_q_jquery
 
 import envi.memcanvas as e_memcanvas
 
 
-class LoggerPage(QtWebEngineWidgets.QWebEnginePage):
-    def javaScriptConsoleMessage(self, msg, line, source):
-        print('%s line %d: %s' % (source, line, msg))
-
-
-class VQMemoryCanvas(e_memcanvas.MemoryCanvas, QtWebEngineWidgets.QWebEngineView):
+class VQMemoryCanvas(e_memcanvas.MemoryCanvas, QtWidgets.QWidget):
     def __init__(self, mem, syms=None, parent=None):
         e_memcanvas.MemoryCanvas.__init__(self, mem, syms=syms)
-        QtWebEngineWidgets.QWebEngineView.__init__(self, parent=parent)
+        QtWidgets.QWidget.__init__(self, parent=parent)
+
+        self._editor = QtWidgets.QTextEdit(self)
+        vbox = QtWidgets.QVBoxLayout()
+        vbox.addWidget(self._editor)
+        self.setLayout(vbox)
 
         self._canv_cache = None
         self._canv_curva = None
-        self._canv_rendtagid = '#memcanvas'
         self._canv_rend_middle = False
-
-        # self._page = LoggerPage()
-        # self._web_channel = QtWebChannel.QWebChannel(self._page)
-        # self.setPage(self._page)
-        # self.page().setWebChannel(self._web_channel)
-
-        htmlpage = e_q_html.template.replace('{{{jquery}}}', e_q_jquery.jquery_2_1_0)
-        self.setContent(htmlpage.encode())
-
-        # self.page().runJavaScript(e_q_jquery.jquery_2_1_0)
-        # self._web_channel.registerObject('vnav', self)
-        ## self.page().addToJavaScriptWindowObject('vnav', self)
-        # self.page().contentsSizeChanged.connect(self._frameContentsSizeChanged)
 
         # Allow our parent to handle these...
         self.setAcceptDrops(False)
-
-    @QtCore.pyqtSlot(str)
-    def showMessage(self, message):
-        print("Message from website:", message)
 
     def renderMemory(self, va, size, rend=None):
 
@@ -64,23 +39,14 @@ class VQMemoryCanvas(e_memcanvas.MemoryCanvas, QtWebEngineWidgets.QWebEngineView
 
         return ret
 
-    def _frameContentsSizeChanged(self, size):
-        if self._canv_scrolled:
-            frame = self.page().mainFrame()
-            frame.setScrollBarValue(QtCore.Qt.Vertical, 0x0fffffff)
-
     @idlethread
     def _scrollToVa(self, va):
         eatevents()  # Let all render events go first
-        # self.page().mainFrame().scrollToAnchor('viv:0x%.8x' % va)
-        ## self._selectVa(va)
+        self._selectVa(va)
 
     @idlethread
     def _selectVa(self, va):
         return
-        # frame = self.page().mainFrame()
-        # frame.evaluateJavaScript('selectva("0x%.8x")' % va)
-        # frame.evaluateJavaScript('scrolltoid("a_%.8x")' % va)
 
     def _beginRenderMemory(self, va, size, rend):
         self._canv_cache = ''
@@ -90,26 +56,17 @@ class VQMemoryCanvas(e_memcanvas.MemoryCanvas, QtWebEngineWidgets.QWebEngineView
         self._canv_cache = None
 
     def _beginRenderVa(self, va):
-        self._add_raw('<a name="viv:0x%.8x" id="a_%.8x">' % (va, va))
+        # self._add_raw('\n')
+        pass
 
     def _endRenderVa(self, va):
-        self._add_raw('</a>')
+        # self._add_raw('\n')
+        pass
 
     def _beginUpdateVas(self, valist):
-
         self._canv_cache = ''
-        # frame = self.page().mainFrame()
-        # elem = frame.findFirstElement('a#a_%.8x' % valist[0][0])
-        # elem.prependOutside('<update id="updatetmp"></update>')
-
-        # for va, size in valist:
-        #     elem = frame.findFirstElement('a#a_%.8x' % va)
-        #     elem.removeFromDocument()
 
     def _endUpdateVas(self):
-        # elem = self.page().mainFrame().findFirstElement('update#updatetmp')
-        # elem.appendOutside(self._canv_cache)
-        # elem.removeFromDocument()
         self._canv_cache = None
 
     def _beginRenderPrepend(self):
@@ -117,9 +74,6 @@ class VQMemoryCanvas(e_memcanvas.MemoryCanvas, QtWebEngineWidgets.QWebEngineView
         self._canv_ppjump = self._canv_rendvas[0][0]
 
     def _endRenderPrepend(self):
-        # frame = self.page().mainFrame()
-        # elem = frame.findFirstElement(self._canv_rendtagid)
-        # elem.prependInside(self._canv_cache)
         self._canv_cache = None
         self._scrollToVa(self._canv_ppjump)
 
@@ -127,9 +81,6 @@ class VQMemoryCanvas(e_memcanvas.MemoryCanvas, QtWebEngineWidgets.QWebEngineView
         self._canv_cache = ''
 
     def _endRenderAppend(self):
-        # frame = self.page().mainFrame()
-        # elem = frame.findFirstElement(self._canv_rendtagid)
-        # elem.appendInside(self._canv_cache)
         self._canv_cache = None
 
     def getNameTag(self, name, typename='name'):
@@ -137,43 +88,16 @@ class VQMemoryCanvas(e_memcanvas.MemoryCanvas, QtWebEngineWidgets.QWebEngineView
         Return a "tag" for this memory canvas.  In the case of the
         vdb tags, they are a tuple of html text (<opentag>, <closetag>)
         """
-        clsname = 'envi-%s' % typename
-        namehex = name.lower()
-        subclsname = 'envi-%s-%s' % (typename, namehex)
-        return ('<span class="%s %s" envitag="%s" envival="%s" '
-                'onclick="nameclick(this)">' % (clsname, subclsname, typename, namehex), '</span>')
+        return "", ""
 
     def getVaTag(self, va):
         # The "class" will be the same that we get back from goto event
-        return ('<span class="envi-va envi-va-0x%.8x" va="0x%.8x" '
-                ' ondblclick="vagoto(this)" oncontextmenu="vaclick(this)" '
-                ' onclick="vaclick(this)">' % (va, va), '</span>')
-
-    @QtCore.pyqtSlot(str)
-    def _jsGotoExpr(self, expr):
-        # The routine used by the javascript code to trigger nav events
-        if self._canv_navcallback:
-            self._canv_navcallback(expr)
-
-    @QtCore.pyqtSlot(str)
-    def _jsSetCurVa(self, vastr):
-        self._canv_curva = int(str(vastr), 0)
+        return "", ""
 
     # NOTE: doing append / scroll separately allows render to catch up
     @idlethread
     def _appendInside(self, text):
-        return
-        # frame = self.page().mainFrame()
-        page = self.page()
-
-        def printpage(content):
-            print(content)
-
-        page.toHtml(printpage)
-        # page.runJavaScript()
-        # elem = self.page().obj.findChild(self._canv_rendtagid)
-        # elem = frame.findFirstElement(self._canv_rendtagid)
-        # elem.appendInside(text)
+        self._editor.insertPlainText(text)
 
     def _add_raw(self, text):
         # If we are in a call to renderMemory, cache til the end.
@@ -186,7 +110,6 @@ class VQMemoryCanvas(e_memcanvas.MemoryCanvas, QtWebEngineWidgets.QWebEngineView
     def addText(self, text, tag=None):
         if isinstance(text, int):
             text = '%x' % text
-        text = html.escape(text)
 
         if tag is not None:
             otag, ctag = tag
@@ -196,14 +119,9 @@ class VQMemoryCanvas(e_memcanvas.MemoryCanvas, QtWebEngineWidgets.QWebEngineView
 
     @idlethreadsync
     def clearCanvas(self):
-        # --CLEAR--
-        return
-        # frame = self.page().mainFrame()
-        # elem = frame.findFirstElement(self._canv_rendtagid)
-        # elem.setInnerXml('')
+        self._editor.clear()
 
     def contextMenuEvent(self, event):
-
         va = self._canv_curva
         menu = QtWidgets.QMenu()
         if self._canv_curva:
@@ -211,7 +129,6 @@ class VQMemoryCanvas(e_memcanvas.MemoryCanvas, QtWebEngineWidgets.QWebEngineView
 
         viewmenu = menu.addMenu('view   ')
         viewmenu.addAction("Save frame to HTML", ACT(self._menuSaveToHtml))
-
         menu.exec_(event.globalPos())
 
     def initMemWindowMenu(self, va, menu):
